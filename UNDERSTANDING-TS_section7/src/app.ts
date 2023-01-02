4cf2c723f945beb680e9681a8a1119d11547b418
NepiaaaaN <surface095@gmail.com>
2023-01-02 16:07:33 +0900

    feat(app.ts): 96. Generic関数の作成

--- UNDERSTANDING-TS_section7/src/app.ts
+++ UNDERSTANDING-TS_section7/src/app.ts
@@ -10,3 +10,10 @@
 // promise.then(data -> {
 //   data.split(' ');
 // });
+
+function merge<T extends {}, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+console.log(mergedObj.age);
