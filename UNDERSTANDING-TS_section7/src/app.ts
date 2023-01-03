d92e72bf3a5911869f81f9d8fa4948d903c574e8
NepiaaaaN <surface095@gmail.com>
2023-01-03 16:05:35 +0900

    feat(app.ts): 100. Generic クラス

--- UNDERSTANDING-TS_section7/src/app.ts
+++ UNDERSTANDING-TS_section7/src/app.ts
@@ -42,3 +42,38 @@ const extractAndConvert = <T extends object, U extends keyof T>(
 };
 
 console.log(extractAndConvert({ name: "Max" }, "name"));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1); // -1を返す
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Data1");
+textStorage.addItem("Data2");
+textStorage.removeItem("Data1");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const obj = { name: "Max" };
+// objStorage.addItem(obj);
+// objStorage.addItem({ name: "Manu" });
+// // ...
+// objStorage.removeItem({ name: "Manu" });
+// console.log(objStorage.getItems());
