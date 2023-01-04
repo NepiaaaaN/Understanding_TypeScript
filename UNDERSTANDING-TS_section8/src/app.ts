62a3b2e18cc52e48c3191c31d7d6b95defd9d5ff
NepiaaaaN <surface095@gmail.com>
2023-01-04 11:06:59 +0900

    feat(app.ts README.md): 110. プロパティデコレータの詳細

--- UNDERSTANDING-TS_section8/src/app.ts
+++ UNDERSTANDING-TS_section8/src/app.ts
@@ -32,3 +32,33 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// ---
+
+const Log = (target: any, propertyName: string | Symbol) => {
+  console.log("Property デコレータ");
+  console.log(target, propertyName);
+};
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("不正な価格です - 0以下は設定出来ません");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
