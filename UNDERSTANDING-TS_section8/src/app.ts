f345545b61990970d38060f78d2548daf1f02c0a
NepiaaaaN <surface095@gmail.com>
2023-01-04 11:21:56 +0900

    feat(app.ts): 111. アクセサとパラメータのデコレータ

--- UNDERSTANDING-TS_section8/src/app.ts
+++ UNDERSTANDING-TS_section8/src/app.ts
@@ -40,11 +40,37 @@ const Log = (target: any, propertyName: string | Symbol) => {
   console.log(target, propertyName);
 };
 
+const Log2 = (target: any, name: string, descriptor: PropertyDescriptor) => {
+  console.log("Accssor デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+};
+
+const Log3 = (
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) => {
+  console.log("Method デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+};
+
+const Log4 = (target: any, name: string | Symbol, position: number) => {
+  console.log("Parameter デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+};
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -58,7 +84,8 @@ class Product {
     this._price = p;
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax);
   }
 }
