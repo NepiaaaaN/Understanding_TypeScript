2ce2ebee6f221cc6336fc53a8c9e93bd0a84fe20
NepiaaaaN <surface095@gmail.com>
2023-01-04 16:55:00 +0900

    feat(app.ts): 124. DOM要素の操作

--- UNDERSTANDING-TS_section9/src/app.ts
+++ UNDERSTANDING-TS_section9/src/app.ts
@@ -1,8 +1,11 @@
-// Code goes here!
 class ProjectInput {
+  // Code goes here!
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -15,9 +18,34 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    // 取得したDOMに対してidを付与
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title" // TODO ここ、#なしで出来るのか試す
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector(
+      "#manday"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  // イベントリスナの設定
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  // 要素を追加
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
