d3498b2b1d5be134aa77eec48b44b950b687531a
NepiaaaaN <surface095@gmail.com>
2023-01-26 09:12:58 +0900

    feat(app.ts): 173. ユーザ入力の取得

--- UNDERSTANDING-TS_section13/src/app.ts
+++ UNDERSTANDING-TS_section13/src/app.ts
@@ -1,1 +1,11 @@
-// ここにコードを書きます
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const searchAddressHandler = (event: Event) => {
+  event.preventDefault(); // formがHTTPリクエストを送らないように設定
+  const enteredAddress = addressInput.value;
+
+  // Google API に送信
+};
+
+form.addEventListener("submit", searchAddressHandler);
